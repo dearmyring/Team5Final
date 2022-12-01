0da664255cf1e4399839bf6018cf420488e2a02d
1009god <1009god3@naver.com>
2022-12-01 13:09:03 +0900

    final 프로젝트 준비

---
+++ Last-dance/src/main/java/com/kh/pj/LastDanceApplication.java
@@ -0,0 +1,13 @@
+package com.kh.pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastDanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastDanceApplication.class, args);
+	}
+
+}
---
+++ Last-dance/src/test/java/com/kh/pj/LastDanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastDanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
