c7390f4399d505fec671eaec26be00bde9de3241
1009god <1009god3@naver.com>
2022-12-01 18:27:10 +0900

    mapper 작성중

--- Last-dance/src/main/java/com/kh/pj/repository/BoardDao.java
+++ Last-dance/src/main/java/com/kh/pj/repository/BoardDao.java
@@ -1,5 +1,13 @@
 package com.kh.pj.repository;
 
-public interface BoardDao {
+import java.util.List;
 
+import com.kh.pj.entity.BoardDto;
+
+public interface BoardDao {
+	void insert(BoardDto boardDto);
+	List<BoardDto> selectList();
+	BoardDto selectOne(int boardNo);
+	boolean edit(BoardDto boardDto);
+	boolean delete(int boardDto);
 }
--- Last-dance/src/main/java/com/kh/pj/repository/BoardDaoImpl.java
+++ Last-dance/src/main/java/com/kh/pj/repository/BoardDaoImpl.java
@@ -1,5 +1,45 @@
 package com.kh.pj.repository;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.pj.entity.BoardDto;
+
+@Repository
 public class BoardDaoImpl implements BoardDao {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public void insert(BoardDto boardDto) {
+		sqlSession.insert("board.insert",boardDto);
+	}
+	
+	@Override
+	public List<BoardDto> selectList() {
+		return sqlSession.selectList("board.list");
+	}
+	
+	
+	@Override
+	public BoardDto selectOne(int boardNo) {
+		return sqlSession.selectOne("board.get",boardNo);
+	}
+
+	@Override
+	public boolean edit(BoardDto boardDto) {
+		
+		return false;
+	}
+
+	@Override
+	public boolean delete(int boardDto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
