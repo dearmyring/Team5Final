5c450e7056a9d9bd5bcb203e25225bbe1922103c
smy984 <smy984@naver.com>
2022-12-02 10:24:59 +0900

    관리자 로그인 기능 구현중

---
+++ Last-dance/src/main/java/com/kh/pj/controller/AdminController.java
@@ -0,0 +1,17 @@
+package com.kh.pj.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.pj.repository.AdminDao;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+	@Autowired
+	private AdminDao adminDao;
+	
+//	@GetMapping("/")
+}
