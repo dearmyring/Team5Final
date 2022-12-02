3b77f0bdb1ee4f7fb59549aaeec4d3e254f10ce9
NamYounMyeong <dbsaud3112@naver.com>
2022-12-02 21:27:41 +0900

    1202 마이페이지 진행중

--- Last-dance/src/main/java/com/kh/pj/controller/MypageController.java
+++ Last-dance/src/main/java/com/kh/pj/controller/MypageController.java
@@ -1,10 +1,17 @@
 package com.kh.pj.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/mypage")
 public class MypageController {
+	
+	@GetMapping("/list")
+	public String mypageHome() {
+		
+		return "mypage";
+	}
 
 }
---
+++ Last-dance/src/main/webapp/WEB-INF/views/mypage.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<jsp:include page="/WEB-INF/views/template/header.jsp">
+	<jsp:param name="title" value="메인페이지"/>
+</jsp:include>
+
+<h2>Test</h2>
+
+<jsp:include page="/WEB-INF/views/template/footer.jsp"></jsp:include>
