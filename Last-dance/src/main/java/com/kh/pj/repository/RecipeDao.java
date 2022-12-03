bc8ee00170fe15de59ae1c1d5bd267904ba253b1
JENN-YOO <hyali6629@naver.com>
2022-12-03 15:55:44 +0900

    레시피 관련  entity 추가  - 리스트 뽑기위한 준비, 추가vo및 관련 impl필요

--- Last-dance/src/main/java/com/kh/pj/repository/RecipeDao.java
+++ Last-dance/src/main/java/com/kh/pj/repository/RecipeDao.java
@@ -1,5 +1,20 @@
 package com.kh.pj.repository;
 
+import java.util.List;
+
+import com.kh.pj.entity.RecipeDto;
+
 public interface RecipeDao {
+	//레시피 등록
+	void insert(RecipeDto recipeDto);
+	
+	//레시피 수정
+	boolean update(int recipeNo);
+	
+	//레시피 조회
+	List<RecipeDto> recipeList(int recipeNo);
+	
+	//레시피 삭제
+	boolean delete(int recipeNo);
 
 }
--- Last-dance/src/main/java/com/kh/pj/repository/RecipeIngredientDaoImpl.java
+++ Last-dance/src/main/java/com/kh/pj/repository/RecipeIngredientDaoImpl.java
@@ -1,5 +1,48 @@
 package com.kh.pj.repository;
 
-public class RecipeIngredientDaoImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.kh.pj.entity.RecipeIngredientDto;
+
+public class RecipeIngredientDaoImpl implements RecipeIngredientDao {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public void insert(RecipeIngredientDto recipeIngredientDto) {
+		String sql = "insert into recipe_ingredient "
+					+ "recipe_ingredient_name "
+						+"values(?)";
+		Object[] param = {recipeIngredientDto.getRecipeIngredientName(),
+										recipeIngredientDto.getRecipeNo()
+		};
+		jdbcTemplate.update(sql, param);	
+	}
+
+	@Override
+	public boolean update(String recipeIngredientName) {
+		String sql = "update recipe_ingredient set "
+					+ "recipe_ingredient_name "
+						+ "where recipe_no=?";
+		Object[] param = {recipeIngredientName};
+		return jdbcTemplate.update(sql, param) > 0;
+	}
+
+	@Override
+	public List<RecipeIngredientDto> RecipeIngredientList(String recipeIngredientName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(String recipeIngredientName) {
+		String sql = "delete recipe_ingredient where recipe_ingredient_name=?";
+		Object[] param = {recipeIngredientName};
+		return jdbcTemplate.update(sql, param) > 0;
+	}
 
 }
