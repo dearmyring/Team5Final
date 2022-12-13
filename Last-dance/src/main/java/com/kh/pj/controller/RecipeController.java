a0898bc14fb73a2c9670723b4742abf3d2518638
NAMYOUNMYEONG <dbsaud3112@naver.com>
2022-12-13 09:58:28 +0900

    콘솔 확인코드 주석처리

--- Last-dance/src/main/java/com/kh/pj/controller/RecipeController.java
+++ Last-dance/src/main/java/com/kh/pj/controller/RecipeController.java
@@ -49,11 +49,11 @@ public class RecipeController {
 			
 			//내가 본 레시피 등록
 			recipeDao.insertRecipeView(recipeViewDto);
-			System.err.println("등록 실행");
+//			System.err.println("등록 실행");
 		}
 		else {
 			recipeDao.updateRecipeViewTime(recipeViewDto);
-			System.err.println("수정 실행");
+//			System.err.println("수정 실행");
 		}
 		
 		model.addAttribute("recipe", recipeDao.selectDetail(recipeNo));
