8e44eb83bce1e0209426adc3218ee9644db7dc77
JENN-YOO <hyali6629@naver.com>
2022-12-22 04:35:18 +0900

    검색 정렬 구현(더보기 테스트해야하는데 size가 안잡힘, 재료 불러오기다시해야) - ++ 리스트, 검색리스트 정렬

--- Last-dance/src/main/java/com/kh/pj/restcontroller/RecipeRestController.java
+++ Last-dance/src/main/java/com/kh/pj/restcontroller/RecipeRestController.java
@@ -19,8 +19,11 @@ import com.kh.pj.entity.RecipeDto;
 import com.kh.pj.entity.RecipeLikeDto;
 import com.kh.pj.repository.AdminDao;
 import com.kh.pj.repository.RecipeDao;
+import com.kh.pj.repository.SearchDao;
 import com.kh.pj.vo.ListSearchVO;
 import com.kh.pj.vo.ListSearchVO1;
+import com.kh.pj.vo.RecipeIngredientVO;
+import com.kh.pj.vo.RecipeListSearchResultMapVO;
 import com.kh.pj.vo.RecipeListVO;
 
 @CrossOrigin(origins = "http://127.0.0.1:5500")
@@ -32,6 +35,8 @@ public class RecipeRestController {
 	private RecipeDao recipeDao;
 	@Autowired
 	private AdminDao adminDao;
+	@Autowired
+	private SearchDao searchDao;
 	
 	@PostMapping("/recipe")
 	public List<RecipeDto> adminList(
@@ -98,6 +103,20 @@ public class RecipeRestController {
 //		vo1.setHavePrev(vo1.hasPrev());
 //		vo1.setHaveNext(vo1.hasNext());	
 		return vo1;
+	}
+	
+	@PostMapping("/recipe2")
+	public List<RecipeListSearchResultMapVO> complexSearch(@RequestBody RecipeIngredientVO recipeIngredientVO){
+		recipeIngredientVO.setCount(searchDao.recipeSearchPostCount(recipeIngredientVO));
+		recipeIngredientVO.setStartPost(recipeIngredientVO.startRow());
+		recipeIngredientVO.setEndPost(recipeIngredientVO.endRow());
+		return searchDao.complexSearch(recipeIngredientVO);
+	}
+	
+	@PostMapping("/recipe-search-count")
+	public RecipeIngredientVO recipeSearchPostCount(@RequestBody RecipeIngredientVO recipeIngredientVO) {
+		recipeIngredientVO.setCount(searchDao.recipeSearchPostCount(recipeIngredientVO));
+		return recipeIngredientVO;
 	}
 
 }
